512b7d13e7bdb353b1b33fcfa3e7ceb5f0ab6c74
SaboteurInCave <saboteurincave@gmail.com>
2016-04-26 00:22:49 +0300

    Fixed empty source view in getSource.php

--- site/getSource.php
+++ site/getSource.php
@@ -10,9 +10,26 @@
         $res = mysqli_query($link, $query);
         if (mysqli_num_rows($res) == 1 && !(($file = @file_get_contents("./executions/".$id)) === FALSE))
         {
-            $file = "<code>" . nl2br(str_replace(" ", "&nbsp;", 
-                str_replace("\t", "    ", htmlspecialchars($file)))) . "</code>";
-            echo $file;
+            
+            // in php >= 5.6 we need to set encoding for htmlspecialchars!
+            $encodings = array('UTF-8', 'CP1251', 'ISO-8859-1', 'KOI8-R');
+            
+            foreach ($encodings as $encoding)
+            {
+               
+                $editedCode = nl2br(str_replace(" ", "&nbsp;", 
+                    str_replace("\t", "    ", htmlspecialchars($file, ENT_COMPAT | ENT_HTML401, $encoding))));
+                    
+                    
+                if (!empty($editedCode)) 
+                {
+                    echo '<code>' . $editedCode . '</code>';
+                    break;
+                }
+
+                
+            }
+
         }
     }
 ?>
